e98e9d766e48ef8be830fd2e36263d9959618546
Jariphiya <68010130@kmitl.ac.th>
2026-03-02 18:38:21 +0700

    mimimimimmimim2

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { MissionService} from './mission.service'
 
+@Controller('mission')
 export class MissionController{
-    
+    constructor(private readonly missionService: MissionService){}
 
+@Get()
+getSummary():{ACTIVE:number; COMPLETED:number; FAILED:number}{
+    return this.missionService.getSummary()
+}
     
 }
